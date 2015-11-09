ca9212dcea206f17a2261a1d495b7b850d755b46
Stanley Darmawan <stanmation@gmail.com>
2015-11-09 22:48:17 +1100

    Initial Commit

---
+++ SpriteDestroy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpriteDestroy
+//
+//  Created by Stanley Darmawan on 9/11/2015.
+//  Copyright © 2015 Stanley Darmawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
